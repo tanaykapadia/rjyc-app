a0e3fbb0c549c4e83e2cee74a007ec749f7dd1ad
Tanay Kapadia <73399373+tanaykapadia@users.noreply.github.com>
2021-03-20 17:23:55 +0530

    Delete www directory

--- www/js/index.js
+++
@@ -1,6 +0,0 @@
-function onDeviceReady(){
-    screen.orientation.lock('portrait'); // Locking the orientation of the screen as the design breaks in landscape mode
-}
-document.addEventListener('deviceready',function(){ // 'Listens' for when the device is ready and the app is open
-    onDeviceReady() // Calling the previous function
-});
